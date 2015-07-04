2ef5d04d752d7d0f51571327236150a24a1d329f
Jason Harrelson <jason@lookforwardenterprises.com>
2015-07-03 21:45:04 -0500

    Add auto reloading of env file.

--- lib/dotenv/server.ex
+++ lib/dotenv/server.ex
@@ -2,12 +2,16 @@ defmodule Dotenv.Server do
   @moduledoc false
   use GenServer
 
+  @reload_timeout_in_minutes 5
+
   def start_link(env_path) do
     :gen_server.start_link({:local, :dotenv}, __MODULE__, env_path, [])
   end
 
   def init(env_path) do
+    ensure_file_exists(env_path)
     env = Dotenv.load!(env_path)
+    Process.send_after self, :auto_reload, reload_timeout_in_ms
     {:ok, env}
   end
 
@@ -26,4 +30,30 @@ defmodule Dotenv.Server do
   def handle_call({:get, key, fallback}, _from, env) do
     {:reply, Dotenv.Env.get(env, fallback, key), env}
   end
+
+  def handle_info(:auto_reload, env) do
+    env = Dotenv.load!(env.paths)
+    Process.send_after self, :auto_reload, reload_timeout_in_ms
+    {:noreply, env}
+  end
+
+  defp reload_timeout_in_ms do
+    @reload_timeout_in_minutes * 60 * 1000
+  end
+
+  defp ensure_file_exists(filename) do
+    filename = case filename do
+      :automatic -> ".env"
+      _          -> filename
+    end
+
+    case File.exists?(filename) do
+      false ->
+        IO.puts "Env file #{filename} does not exist."
+        :timer.sleep(1000)
+        System.halt(1)
+      true  -> true
+    end
+  end
+
 end
